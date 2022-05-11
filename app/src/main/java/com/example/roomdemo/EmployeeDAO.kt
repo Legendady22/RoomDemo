ff8e0ae1aecff2a144826ed33ba43ed672a32085
Legendady <kjtbolts22@gmail.com>
2022-05-11 00:07:59 -0600

    Finished connection to DB. Created layouts for main page, Edit Dialog and RecyclerView items. Next Step: Create adapter for recycler view and connect it to the database.

--- app/src/main/java/com/example/roomdemo/EmployeeDAO.kt
+++ app/src/main/java/com/example/roomdemo/EmployeeDAO.kt
@@ -1,8 +1,27 @@
 package com.example.roomdemo
 
-import androidx.room.Dao
+import androidx.room.*
+import java.util.concurrent.Flow
 
 
 @Dao
 interface EmployeeDAO {
+
+    @Insert
+    suspend fun insert(employeeEntity: EmployeeEntity)
+
+    @Update
+    suspend fun update(employeeEntity: EmployeeEntity)
+
+    @Delete
+    suspend fun delete(employeeEntity: EmployeeEntity)
+
+    @Query("SELECT * FROM `employee-table`")
+    fun fetchAllEmployees():kotlinx.coroutines.flow.Flow<List<EmployeeEntity>>
+
+    @Query("SELECT * FROM `employee-table` WHERE id=:id")
+    fun fetchEmployeeById(id:Int):kotlinx.coroutines.flow.Flow<EmployeeEntity>
+
+
+
 }
